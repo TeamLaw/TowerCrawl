5944762408d458ee8ca406b240c972175c27c66f
papa0four <papa.o.deuce@gmail.com>
2017-09-25 14:31:41 -0500

    got rid of extra defines

--- gameInteractions.c
+++ gameInteractions.c
@@ -1,12 +1,4 @@
 #include "TowerCrawl.h"
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
 
 
 //DrawEncounters will display the the health of both the monster and the player
