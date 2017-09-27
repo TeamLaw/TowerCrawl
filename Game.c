4d5389f511dbae65818df532ee5138facba6c8cb
Build a better bonsai <neumann6994@gmail.com>
2017-09-27 13:13:45 -0500

    Comments add (#66)
    * added comments
    
    * Update Game.c

--- Game.c
+++ Game.c
@@ -29,26 +29,43 @@ int main()
 
 	}
 	clearMemory();
-	
+
 	return 0;
 }
 
+/*
+randomNum(int low, int high) generates a random number
+Parameters:
+low - The lowest possible random number
+high- The highest possible random number
+Returns:
+the random number
+*/
 int coordCompare(COORD coord1, COORD coord2)//OVERLOAD OPERATORS IN C++!!!
 {
 	return (coord1.X == coord2.X && coord1.Y == coord2.Y);
 }
 
+/*
+randomNum(int low, int high) generates a random number
+Parameters:
+low - The lowest possible random number
+high- The highest possible random number
+Returns:
+the random number
+*/
 void moveCursor(int x, int y)
 {
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), (COORD) { x, y });
 }
 
-/*randomNum(int low, int high) generates a random number
-Parameters: 
-	low - The lowest possible random number 
-	high- The highest possible random number
-Returns: 
-	the random number
+/*
+randomNum(int low, int high) generates a random number
+Parameters:
+low - The lowest possible random number
+high- The highest possible random number
+Returns:
+the random number
 */
 int randomNum(int low, int high)
 {
@@ -58,17 +75,31 @@ int randomNum(int low, int high)
 	return r;
 }
 
+/*
+randomNum(int mod) Compares how many rooms have been entered with how many are left, if all have been entered, spawn boss or continue spawning rooms
+Parameters:
+mod - edits it behavior
+Returns:
 
+Programmer: Law
+*/
 int minCheck(int mod)
-{//Compares how many rooms have been entered with how many are left, if all have been entered, spawn boss or continue spawning rooms
+{
 	int counter = mod;
-  
+
 	for (int i = 0; i < delCounter; i++) { counter += delPointers[i]->entered; }
-	
+
 	return (counter == delCounter);
 }
 
-
+/*
+clearMemory() At the end of the program, this function frees up all the memory
+Parameters:
+None
+Returns:
+Void
+Programmer: Law
+*/
 void clearMemory()
 {
 	for (int i = 0; i < delCounter; i++)
@@ -76,33 +107,42 @@ void clearMemory()
 	delCounter = 0;
 }
 
-void printGameIntroMessage()
-{
-	printf("============= \n");
-	printf("WELCOME TO    \n");
-	printf("============= \n");
-
-	printf("\n-`-`-`-``-`-` \n");
-	printf("  T		   \n");
-	printf("  0		   \n");
-	printf("  W		   \n");
-	printf("  E		   \n");
-	printf("C R A W L  \n\n");
-
-	printf("PRESS ANY KEY TO START THE GAME \n");
-	printf("> ");
-	_getch();
-	system("cls");
-
-	printf("~ An evil wizard has stolen your crown. ~\n~ You and your finest warriors travel to the wizard's tower ~\n~ to take it back! ~ \n>");	
-	_getch();
-	system("cls");
-	
-	printf("~ You burst down the front door of his tower. ~\n~ The room goes dark and you find yourself alone, ~\n~ in a strange room.... ~ \n>");
-	_getch();
-	system("cls");
-	
-	printf("~ Doors appear on the walls. The wizard's strange ~\n~ magic will not... cannot keep you from your goal. Travel to the ~\n~ top of the tower and end this maddness! ~ \n>");
-	_getch();
-	system("cls");
-}
+/*
+printGameIntroMessage() Handles the intro message and art
+Parameters:
+None
+Returns:
+Void
+Programmer: Law
+*/
+ void printGameIntroMessage()
+ {		 {
+-	printf("============= \n");		+	printf("TO ADD GRAPHIC\nPRESS ANY KEY TO START THE GAME \n");
+-	printf("WELCOME TO    \n");		
+-	printf("============= \n");		
+-		
+-	printf("\n-`-`-`-``-`-` \n");		
+-	printf("  T		   \n");		
+-	printf("  0		   \n");		
+-	printf("  W		   \n");		
+-	printf("  E		   \n");		
+-	printf("C R A W L  \n\n");		
+-		
+-	printf("PRESS ANY KEY TO START THE GAME \n");		
+ 	printf("> ");		 	printf("> ");
+ 	_getch();		 	_getch();
+ 	system("cls");		 	system("cls");
+ 		 
+-	printf("~ An evil wizard has stolen your crown. ~\n~ You and your finest warriors travel to the wizard's tower ~\n~ to take it back! ~ \n>");			+	printf("~ An evil wizard has stolen your crown. ~\n~ You and your finest warriors travel to the wizard's tower ~\n~ to take it back! ~ \n>");
+ 	_getch();		 	_getch();
+ 	system("cls");		 	system("cls");
+-			+
+ 	printf("~ You burst down the front door of his tower. ~\n~ The room goes dark and you find yourself alone, ~\n~ in a strange room.... ~ \n>");		 	printf("~ You burst down the front door of his tower. ~\n~ The room goes dark and you find yourself alone, ~\n~ in a strange room.... ~ \n>");
+ 	_getch();		 	_getch();
+ 	system("cls");		 	system("cls");
+-			+
+-	printf("~ Doors appear on the walls. The wizard's strange ~\n~ magic will not... cannot keep you from your goal. Travel to the ~\n~ top of the tower and end this maddness! ~ \n>");		+	printf("~ Doors appear on the walls. The wizard strange ~\n~ magic will not... cannot keep you from your goal. Travel to the ~\n~ top of the tower and end this maddness! ~ \n>");
+ 	_getch();		 	_getch();
+ 	system("cls");		 	system("cls");
++
+ } 		 }
